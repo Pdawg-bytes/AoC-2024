2645ac44cc7acb95bc67f2f8d3caa941150de2ce
Pdawg11239 <83825746+Pdawg-bytes@users.noreply.github.com>
2024-12-15 22:33:53 -0500

    Day 15 p1

--- AoC2024/AoC2024/Puzzles/Day15.cs
+++ AoC2024/AoC2024/Puzzles/Day15.cs
@@ -9,20 +9,29 @@ namespace AoC2024.Puzzles
 
         static char[][] map;
 
+        enum CollisionType
+        {
+            Box,
+            Wall,
+            None,
+        }
+
         public string FindAnswer(byte part)
         {
-            string[] split = DAY15_TEST.Split("\r\n\r\n");
+            string[] split = DAY15_INPUT.Split("\r\n\r\n");
             map = split[0].Split("\r\n").Select(row => row.ToCharArray()).ToArray();
 
-            List<Vector> movements = split[1].Select(c => c switch
-            {
-                '<' => (-1, 0),
-                '>' => (1, 0),
-                '^' => (0, -1),
-                'v' => (0, 1),
-            }).ToList();
+            List<Vector> movements = split[1]
+                .Split("\r\n")
+                .SelectMany(line => line.Select(c => c switch
+                {
+                    '<' => new Vector(-1, 0),
+                    '>' => new Vector(1, 0),
+                    '^' => new Vector(0, -1),
+                    'v' => new Vector(0, 1),
+                })).ToList();
 
-            (int posX, int posY) startingPos = map
+            Vector robotPosition = map
                 .Select((row, rowIndex) => new { row, rowIndex })
                 .Where(x => x.row.Contains('@'))
                 .Select(x => (x.rowIndex, Array.IndexOf(x.row, '@'))).First();
@@ -30,20 +39,50 @@ namespace AoC2024.Puzzles
             switch (part)
             {
                 case 1:
-                    for (int i = 0; i < movements.Count; i++)
+                    Vector firstBox;
+                    Vector current;
+                    foreach (var move in movements)
                     {
+                        map[robotPosition.y][robotPosition.x] = '.';
+                        var (collision, pos) = AttemptMovement(robotPosition, move);
+                        switch (collision)
+                        {
+                            case CollisionType.Wall: continue;
+                            case CollisionType.None: { robotPosition = pos; break; }
+                            case CollisionType.Box:
+                                map[robotPosition.y][robotPosition.x] = '.';
+                                current = firstBox = pos;
+                                while (true)
+                                {
+                                    current = (current.x + move.x, current.y + move.y);
+                                    if (map[current.y][current.x] != 'O') break;
+                                }
+                                if (map[current.y][current.x] == '#') continue;
+                                if (map[current.y][current.x] == '.')
+                                {
+                                    robotPosition = firstBox;
+                                    map[firstBox.y][firstBox.x] = '.';
+                                    map[current.y][current.x] = 'O';
+                                }
+                                break;
+                        }
+                        map[pos.y][pos.x] = '@';
                     }
-                    break;
+                    return map
+                        .SelectMany((row, y) => row.Select((cell, x) => (cell, x, y)))
+                        .Where(item => item.cell == 'O')
+                        .Sum(item => 100 * item.y + item.x).ToString();
                 case 2:
                     break;
             }
             return "Unable to find answer!";
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        static (bool, Vector) AttemptMovement(Vector pos, Vector dir)
+        static (CollisionType, Vector) AttemptMovement(Vector pos, Vector dir) => map[pos.y + dir.y][pos.x + dir.x] switch
         {
-            if (map[pos.y + dir.y][pos.x + dir.x] == '#') return (false, pos);
-            return (true, (pos.x + dir.x, pos.y + dir.y));
-        }
+            '#' => (CollisionType.Wall, pos),
+            'O' => (CollisionType.Box, (pos.x + dir.x, pos.y + dir.y)),
+            '.' => (CollisionType.None, (pos.x + dir.x, pos.y + dir.y))
+        };
     }
 }
